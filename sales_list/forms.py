54f06238279e7272a35981b497243e751fd8f913
aebr3 <beatricearapoc@gmail.com>
2024-12-04 19:40:35 +0800

    added views, READ, create new item

---
+++ sales_list/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import MenuItem, OrderDetail
+
+class MenuForm(forms.ModelForm):
+    class Meta:
+        model = MenuItem
+        fields = ['name', 'price']
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = OrderDetail
+        fields = ['menu_item', 'quantity', 'date', 'payment_amount']
